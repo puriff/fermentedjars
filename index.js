0024ca47e5e9d7c4a0ee8bc4a948c52036c3408c
purif <purifavax@protonmail.com>
2023-06-21 11:55:28 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,27 @@
+import {ethers} from "ethers"
+
+const providerArb = new ethers.providers.StaticJsonRpcProvider(
+    "https://arbitrum-one.publicnode.com", 
+    {
+      chainId: 42161,
+      name: "Arbitrum",
+    }
+  );
+
+  let jarABI = [
+    "event FermentedJarsFound(uint8 bundleId, uint256[] honeyJarIds)",
+    ]
+
+
+async function getFermentedJarsID() {
+    let jars = new ethers.Contract("0x37aa84e2b62a18894610227acb4d3c77346d9c27", jarABI, providerArb)
+
+    let currentBlock = await providerArb.getBlockNumber()
+    let events = await jars.queryFilter('FermentedJarsFound', 103248314, currentBlock);
+    for (let index = 0; index < events.length; index++) {
+        let id = events[index].args.honeyJarIds
+        console.log("Fermented jar "+index +" : " +Number(id))
+    }
+}
+
+getFermentedJarsID()
